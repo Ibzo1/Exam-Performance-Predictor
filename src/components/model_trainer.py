99703fb2e28b711aa204e7721ad5be84e570c9fd
Ibraheem Azhar <ibu.azhar@gmail.com>
2023-11-27 16:11:38 -0500

    Tuning Params Added

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -46,8 +46,52 @@ class ModelTrainer:
                 "AdaBoost Regressor": AdaBoostRegressor(),
             }
 
+            params={
+                "Decision Tree": {
+                    'criterion': ['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
+                    # 'splitter':['best','randpm'],
+                    # 'max_features':['sqrt','log2'],
+                },
+                "Random Forest":{
+                    #'criterion':['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
+                    # 'max_features':['sqrt','log2'],
+
+                    'n_estimators': [8,16,32,64,128,256]
+                },
+                "Gradient Boosting":{
+                    #'loss':['squared_error','huber', 'absolute_error', 'quantile'],
+                    'learning_rate':[.1,.01,.05,.001],
+                    'subsample':[0.6,0.7,0.75,0.8,0.85,0.9],
+                    #'criterion':['squared_error', 'friedman_mse'],
+                    # 'max_features':['auto', 'sqrt','log2'],
+                    'n_estimators': [8,16,32,64,128,256]
+                },
+                "Linear Regression":{},
+                "K-Neighbour Regression":{
+                    'n_neighbors': [3,5,7,9],
+                    'weights': ['uniform', 'distance'],
+                    'algorithm': ['auto', 'ball_tree','kd_tree','brute']
+                },
+                "XGBRegressor":{
+                    'learning_rate':[.1,.01,.05,.001],
+                    'n_estimators': [8,16,32,64,128,256]
+                },
+                "CatBoosting Regressor":{
+                    'depth': [6,8,10],
+                    'learning_rate': [0.01, 0.05, 0.1],
+                    'iterations': [30, 50, 100]
+                },
+                "AdaBoost Regressor":{
+                    'learning_rate':[.1,.01,0.5,.001],
+                    # 'loss':['linear','square','exponential'],
+                    'n_estimators': [8,16,32,64,128,256]
+                }
+            }
+            
+
             # Evaluate_models in utils
-            model_report:dict=evaluate_models(x_train=x_train, y_train=y_train,x_test=x_test, y_test=y_test, models=models)
+            model_report:dict=evaluate_models(x_train=x_train, y_train=y_train,x_test=x_test, y_test=y_test, 
+                                              models=models, param=params)
 
 
             # Get best model score and name from final dict.
@@ -62,7 +106,7 @@ class ModelTrainer:
             if best_model_score<0.6:
                 raise CustomException("No best model found")
             
-            logging.info(f"Best found model on both training and testing dataset")
+            logging.info(f"Best found model on both training and testing dataset: {best_model}")
 
 
             save_object(
